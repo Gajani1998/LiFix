adef2ed5bf9756ced49b5801d64b98bbd495e361
Gajani1998 <56734514+Gajani1998@users.noreply.github.com>
2021-02-16 07:58:40 +0530

    Add files via upload

--- storekeeper/returnitem.php
+++ storekeeper/returnitem.php
@@ -1,6 +1,6 @@
 <?php 
 
-include_once '../utils/classloader.php';
+include_once  __DIR__ . '/../utils/classloader.php';
 $session = new classes\Session(StorekeeperFL);
 ?>
 
@@ -17,16 +17,26 @@ $session = new classes\Session(StorekeeperFL);
     <script src="script.js"></script>
     <script src="https://www.google.com/jsapi"></script>	
     <script src="https://kit.fontawesome.com/2b554022ef.js" crossorigin="anonymous"></script>
-    <title>return items</title>
+    <title>Return item Checking</title>
 
 </head>
+
+<style>
+
+.field{
+  display: none;
+  width: 150px;
+  margin: 0;
+}
+
+</style>
 <body>
     <?php include "./views/nav.php" ?>
 
     
     <div class="main_content">
         <header>
-            <h1>Return Items</h1>
+            <h1>Return Item Checking</h1>
         </header>
                 
                     <div class="details">
@@ -70,8 +80,8 @@ $session = new classes\Session(StorekeeperFL);
                                 <td>1</td>
                                 <td>LED BULB</td>
                                 <td>1</td>
-                                <td><button  class="bttn" id="demo" onclick="myFunction()">confirm</button></td>
-                                <td><button class="bttn button1" id="emo" onclick="myFunction1()" >Decline</button></td>
+                                <td><button  class="bttn confirm" id="1" >confirm</button><input class="field declineInput" type="text" placeholder="Enter difference" ></td>
+                                <td><button class="bttn button1 decline" id="emo"  >Decline</button></td>
 
                             </tr>
                             
@@ -79,29 +89,29 @@ $session = new classes\Session(StorekeeperFL);
                                 <td>2</td>
                                 <td>SUNBOXES</td>
                                 <td>2</td>
-                                <td><button  class="bttn" id="demo1" onclick="myFunction2()">confirm</button></td>
-                                <td><button class="bttn button1" id="emo1" onclick="myFunction22()" >Decline</button></td>
+                                <td><button  class="bttn confirm" id="demo1" >confirm</button><input class="field declineInput" type="text" placeholder="Enter difference" ></td>
+                                <td><button class="bttn button1 decline" id="emo1"  >Decline</button></td>
                             </tr>
                             <tr>
                                 <td>3</td>
                                 <td>WIRES</td>
                                 <td>3m</td>
-                                <td><button  class="bttn"  id="demo3" onclick="myFunction3()">confirm</button></td>
-                                <td><button class="bttn button1" id="emo3" onclick="myFunction33()">Decline</button></td>
+                                <td><button  class="bttn confirm"  id="demo3" >confirm</button><input class="field declineInput" type="text" placeholder="Enter difference" ></td>
+                                <td><button class="bttn button1 decline" id="emo3" >Decline</button></td>
                             </tr>
                             <tr>
                                 <td>4</td>
                                 <td>FUSE</td>
                                 <td>4</td>
-                                <td><button  class="bttn" id="demo4" onclick="myFunction4()">confirm</button></td>
-                                <td><button class="bttn button1" id="emo4" onclick="myFunction44()">Decline</button></td>
+                                <td><button  class="bttn confirm" id="demo4" >confirm</button><input class="field declineInput" type="text" placeholder="Enter difference" ></td>
+                                <td><button class="bttn button1 decline" id="emo4" >Decline</button></td>
                             </tr>
                             <tr>
                                 <td>5</td>
                                 <td>BULB</td>
                                 <td>3</td>
-                                <td><button  class="bttn">confirm</button></td>
-                                <td><button class="bttn button1">Decline</button></td>
+                                <td><button  class="bttn confirm">confirm</button><input class="field declineInput" type="text" placeholder="Enter difference" ></td>
+                                <td><button class="bttn button1 decline">Decline</button></td>
                             </tr>
                             
                             
@@ -116,89 +126,48 @@ $session = new classes\Session(StorekeeperFL);
     
                 </div>
      <script>
-function myFunction() {
-    x=document.getElementById("demo");
-    y=document.getElementById("emo");
-    if (x.style.visibility === 'hidden'  ) {
-      x.style.visibility = 'hidden';
-  } else {
-    y.style.visibility = 'hidden';
-    document.getElementById("demo").innerHTML = "Equal";
-  }
-}
-function myFunction1() {
-    x=document.getElementById("demo");
-    y=document.getElementById("emo");
-    if (y.style.visibility === 'hidden' ) {
-      y.style.visibility = 'hidden';
-  } else {
-    x.style.visibility = 'hidden';
-    document.getElementById("emo").innerHTML = "Fraud";
-  }
-}
+       var confirmBtns = document.querySelectorAll('.confirm');
+       var declineBtns = document.querySelectorAll('.decline');
+       var declineInputs = document.querySelectorAll('.declineInput');
 
-function myFunction2() {
-    x=document.getElementById("demo1");
-    y=document.getElementById("emo1");
-    if (x.style.visibility === 'hidden' ) {
-      x.style.visibility = 'hidden';
-  } else {
-    y.style.visibility = 'hidden';
-    document.getElementById("demo1").innerHTML = "Equal";
-  }
-}
-function myFunction22() {
-    x=document.getElementById("demo1");
-    y=document.getElementById("emo1");
-    if (y.style.visibility === 'hidden' ) {
-      y.style.visibility = 'hidden';
-  } else {
-    x.style.visibility = 'hidden';
-    document.getElementById("emo1").innerHTML = "Fraud";
-  }
-}
+        confirmBtns.forEach( (confirmbtn,index) => {
 
-function myFunction3() {
-    x=document.getElementById("demo3");
-    y=document.getElementById("emo3");
-    if (x.style.visibility === 'hidden' ) {
-      x.style.visibility = 'hidden';
-  } else {
-    y.style.visibility = 'hidden';
-    document.getElementById("demo3").innerHTML = "Equal";
-  }
-}
-function myFunction33() {
-    x=document.getElementById("demo3");
-    y=document.getElementById("emo3");
-    if (y.style.visibility === 'hidden' ) {
-      y.style.visibility = 'hidden';
-  } else {
-    x.style.visibility = 'hidden';
-    document.getElementById("emo3").innerHTML = "Fraud";
-  }
-}
+          confirmbtn.addEventListener('click',()=>{
 
-function myFunction4() {
-    x=document.getElementById("demo4");
-    y=document.getElementById("emo4");
-    if (x.style.visibility === 'hidden' ) {
-      x.style.visibility = 'hidden';
-  } else {
-    y.style.visibility = 'hidden';
-    document.getElementById("demo4").innerHTML = "Equal";
-  }
-}
-function myFunction44() {
-    x=document.getElementById("demo4");
-    y=document.getElementById("emo4");
-    if (y.style.visibility === 'hidden' ) {
-      y.style.visibility = 'hidden';
-  } else {
-    x.style.visibility = 'hidden';
-    document.getElementById("emo4").innerHTML = "Fraud";
-  }
-}
+            declinebtn = declineBtns[index];
+          if (confirmbtn.style.visibility === 'hidden'  ) {
+            confirmbtn.style.visibility = 'hidden';
+          } else {
+            declinebtn.style.visibility = 'hidden';
+            confirmbtn.innerHTML = "Equal";
+          }
+
+
+          })
+
+          
+        } )
+
+        declineBtns.forEach( (declinebtn,index) => {
+
+          declinebtn.addEventListener('click',()=>{
+
+            confirmbtn = confirmBtns[index];
+            if (declinebtn.style.visibility === 'hidden' ) {
+              declinebtn.style.visibility = 'hidden';
+            } else {
+              confirmbtn.style.display = 'none';
+              declineInputs[index].style.display = 'block';
+              declinebtn.innerHTML = "Fraud";
+            }
+
+
+          })
+
+          
+        } )
+
+
 </script>
 </body>
 </html>

3def4a16c185f34fcf979d8a4c3f7834844fae28
Gajani1998 <56734514+Gajani1998@users.noreply.github.com>
2021-03-28 07:44:26 +0530

    Update index.php

--- Clerk/index.php
+++ Clerk/index.php
@@ -1,23 +1,42 @@
+<?php
+include_once  __DIR__ . '/../utils/classloader.php';
+$clerck = new classes\Clerck();
+$data =  $clerck->DailyRepair();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="apple-touch-icon" sizes="180x180" href="../assets/favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="../assets/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="../assets/favicon/favicon-16x16.png">
+	<link rel="manifest" href="../assets/favicon/site.webmanifest">
+    <!-- <link rel="stylesheet" href="<?= BASE ?>/css/slider.css">
+    <link rel="stylesheet" href="<?= BASE ?>/css/style.css"> -->
     <link rel="stylesheet" href="../css/slider.css">
-    <link rel="stylesheet" href="./css/clerk.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/clerk/clerk.css">
     <script src="https://kit.fontawesome.com/2b554022ef.js" crossorigin="anonymous"></script>
     <script src='https://api.mapbox.com/mapbox-gl-js/v1.11.0/mapbox-gl.js'></script>
     <link href='https://api.mapbox.com/mapbox-gl-js/v1.11.0/mapbox-gl.css' rel='stylesheet' />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
     <title>Repairs</title>
 
 </head>
 
 <body>
-   
-<?php include "./components/nav.php" ?>
 
-   
+
+
+
+    <?php include "./views/nav.php" ?>
+    <?php $clerck->getSession()->showMessage() ?>
+
+
     <div class="main_content">
         <header>
             <h1>Available Repairs</h1>
@@ -27,7 +46,7 @@
             <div class="list-section">
                 <div class="lists">
                     <!-- load available repairlist from database -->
-                    <?php include "./components/AvailRepairList.php" ?>
+                    <?php include "./views/AvailRepairList.php" ?>
                 </div>
                 <!-- <button style="margin-top: 10px;" onclick="AssignRepairs()">Assign</button> -->
             </div>
@@ -53,36 +72,37 @@
 
 
 
-        // var marke2 = new mapboxgl.Marker()
-        //     .setLngLat([79.854770, 6.891551])
-        //     .addTo(map);
         var xmlhttp = new XMLHttpRequest();
 
         xmlhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
 
                 mapdata = JSON.parse(this.responseText);
-                // console.log(mapdata);
                 // add markers to the map
                 mapdata.forEach(mk => {
-
+                    //for popup to display lamppost id
+                    var popup = new mapboxgl.Popup({
+                            closeButton: false
+                        })
+                        .setHTML("<h2>#" + mk.lp_id + "</h2>")
+                        .addTo(map);
 
                     var marker = new mapboxgl.Marker({
                             color: "black"
                             // color: "#3FB1CE"
                         })
                         .setLngLat([mk.longitude, mk.lattitude])
-                        .addTo(map);
-                    // markerArr['id' + mk.repair_id] = mk;
-                    markerArr.set(mk.repair_id, marker);
+                        .addTo(map).setPopup(popup);;
+                    markerArr.set(mk.repair_id, [mk.longitude, mk.lattitude]);
                 });
 
             }
         };
-        xmlhttp.open("GET", "./components/getMapdata.php", true);
+        xmlhttp.open("GET", "./ajax/getMapdata.php", true);
         xmlhttp.send();
     </script>
-    <script src="app.js"></script>
+    <script src="./../js/clerck/app.js"></script>
 </body>
 
 </html>
+© 
